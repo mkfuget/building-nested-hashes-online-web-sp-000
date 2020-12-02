cd5d93a0a545052ce17259cd909a05e84ebf8f34
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 04:14:10 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {name: "Benvolio", age: "30", status: "confused"},
+        {name: "Benvolio", age: "30", status: "worried"},
         {name: "Mercutio", age: "44", status: "worried"}
       ]
    }, 
