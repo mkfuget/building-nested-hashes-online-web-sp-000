969c954a5079e0328b955dc410f0a162b5f4a1db
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 04:13:28 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -13,8 +13,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {name: "Juliet", age: "15", status: "alive"},
-        {name: "Juliet", age: "15", status: "alive"},
+        {name: "Steven", age: "30", status: "alive"},
+        {name: "Nurse", age: "44", status: "alive"},
         #your hashes here!
       ]
    }
