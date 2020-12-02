01928d160cc745fe829266132058927f00cff4a5
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 04:19:36 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,8 @@ def bonus
    }
   }
 
-  [:montague][:hero][:status] = "dead"
-  [:capulet][:hero][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:hero][:status] = "dead"
 
   
 
